b791c9582f4af20647773ea537181a3b61d2747c
Fredo <fredpoubelle59@gmail.com>
2025-08-25 07:46:25 +0200

    Create quizzes.js

---
+++ quizzes.js
@@ -0,0 +1,10 @@
+export const quizzes = {
+  perigueux: [
+    { lat: 45.187, lng: 0.721, question: "Quelle est la couleur de la porte du monument ?", answer: "rouge" },
+    { lat: 45.188, lng: 0.722, question: "Combien de fenêtres a la façade ?", answer: "3" },
+  ],
+  bordeaux: [
+    { lat: 44.8378, lng: -0.5792, question: "Quel animal est sur la fontaine ?", answer: "cheval" },
+    { lat: 44.8382, lng: -0.5805, question: "Quelle couleur est la place ?", answer: "grise" },
+  ]
+};
